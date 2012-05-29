f83ad3aae87763076412c4c063f183a9a791a301
ssssyssss <cd032@naver.com>
2012-05-29 23:55:37 +0900

    login screen

--- Mafia_game/gen/com/android/mafia_game/R.java
+++ Mafia_game/gen/com/android/mafia_game/R.java
@@ -15,11 +15,17 @@ public final class R {
         public static final int intro_image=0x7f020001;
     }
     public static final class id {
-        public static final int button=0x7f050001;
-        public static final int login=0x7f050000;
+        public static final int Login=0x7f050006;
+        public static final int Sign_Up=0x7f050005;
+        public static final int button1=0x7f050004;
+        public static final int editText1=0x7f050001;
+        public static final int editText2=0x7f050003;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050002;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login_screen=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- Mafia_game/src/com/android/mafia_game/Mafia_gameActivity.java
+++ Mafia_game/src/com/android/mafia_game/Mafia_gameActivity.java
@@ -13,8 +13,15 @@ public class Mafia_gameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button btn = (Button)findViewById(R.id.login);
-        btn.setOnClickListener(new Button.OnClickListener(){
+        Button btn1 = (Button)findViewById(R.id.Sign_Up);
+        btn1.setOnClickListener(new Button.OnClickListener(){
+        	public void onClick(View v){
+        		Intent intent = new Intent(Mafia_gameActivity.this, Sign_Up.class);
+        		startActivity(intent);
+        	}
+        });
+        Button btn2 = (Button)findViewById(R.id.Login);
+        btn2.setOnClickListener(new Button.OnClickListener(){
         	public void onClick(View v){
         		Intent intent = new Intent(Mafia_gameActivity.this, Login.class);
         		startActivity(intent);
