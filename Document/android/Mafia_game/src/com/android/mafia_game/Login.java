adc9bca5370e6720f49ff3b30e9605d72b29c60f
ssssyssss <cd032@naver.com>
2012-05-29 23:38:30 +0900

    log in screen

---
+++ Document/android/Mafia_game/src/com/android/mafia_game/Login.java
@@ -0,0 +1,17 @@
+package com.android.mafia_game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.login_screen);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
